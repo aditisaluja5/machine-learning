7cd999a5c20e8c7545bcae88cb21a3d63ebd059e
Aditi <saluja.aditi5@gmail.com>
2019-01-21 22:56:54 +0530

    done

--- hello.cpp
+++ hello.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int main(){
 
-cout<<"hello Everyone";
-cout<<"Welcome to my first git repo";
-cout<<"bye to all";
+cout<<"hello Everyone"<<endl;
+cout<<"Welcome to my first git repo"<<endl;
+cout<<"bye to all"<<endl;
+cout<<"done with all the greeting"<<endl;
 return 0;
 
 }
